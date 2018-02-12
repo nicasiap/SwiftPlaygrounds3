9fb397b7e2c3be6abd42d5da4328680770afa4f3
CM Student <cmstudent@UHWO-ACM-C-03.local>
2018-02-12 10:17:30 -1000

    ADDED an array and PRINTED strings to accompany it

--- SwiftPlayground3.playground/Contents.swift
+++ SwiftPlayground3.playground/Contents.swift
@@ -3,3 +3,27 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//how to declare a variable
+
+var fruitBasket: [String]
+//square brackets tell us we're declaring an array
+
+//simple string variable
+var simpleString: String
+
+//initialize an array using an array literal
+fruitBasket = ["apples", "apple bananas", "watermelon", "honeydew"]
+
+print (fruitBasket)
+
+//for-in loop
+for name in fruitBasket {
+    
+    //print "I like to eat" before every name in fruit basket
+    print("I like to eat " + name )
+}
+
+//print out item at index 1
+print( fruitBasket[1])
+print ( fruitBasket[0])
